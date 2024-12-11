ce95a5d1156c788bde774443746719f39b5bf659
jastro-dev <t4drummer@gmail.com>
2024-12-10 17:44:32 -0800

    add customer lifetime value query

--- project.sql
+++ project.sql
@@ -217,3 +217,20 @@ SELECT c.contactLastName, c.contactFirstName, c.city, c.country, p.profit
 FROM profits p
     JOIN customers c ON c.customerNumber = p.customerNumber
 LIMIT 5;
+
+-- Customer Lifetime Value (AVG Profit)
+
+WITH
+    profits AS (
+        SELECT o.customerNumber, SUM(
+                quantityOrdered * (priceEach - buyPrice)
+            ) AS profit
+        FROM
+            products p
+            JOIN orderdetails od ON p.productCode = od.productCode
+            JOIN orders o ON o.orderNumber = od.orderNumber
+        GROUP BY
+            o.customerNumber
+    )
+SELECT ROUND(AVG(profit), 2) AS ltv
+FROM profits;
